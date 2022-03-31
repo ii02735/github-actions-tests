519b0921b9d41316bece282e6e5bc450999d2c00
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-31 15:54:40 +0200

    [WORKFLOW] Showing connection information

--- tests/db.test.js
+++ tests/db.test.js
@@ -20,6 +20,16 @@ const path = require('path')
 const fs = require('fs');
 
 test("Adding table into test db", async () => {
+    
+    console.log({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        charset: "utf8mb4",
+    });
+    
     const sql = fs.readFileSync(path.join(__dirname,'resources','table.sql')).toString()
     try{
         await knex.raw(sql);
